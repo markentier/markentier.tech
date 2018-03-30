4773e046bfa644a8c67d4f02d745ef6b49bff665
Christoph Grabo <asaaki@mannaz.cc>
2018-03-31 01:55:17 +0200

    Try custom cache headers

---
+++ site/static/js/offline.js
@@ -0,0 +1,25 @@
+// https://github.com/gokulkrishh/demo-progressive-web-app/blob/master/js/offline.js
+(function () {
+  'use strict';
+
+  const headerElement = document.querySelector('header')
+  const metaTagTheme = document.querySelector('meta[name=theme-color]')
+
+  document.addEventListener('DOMContentLoaded', function(event) {
+    if (!navigator.onLine) { updateNetworkStatus() }
+    window.addEventListener('online', updateNetworkStatus, false)
+    window.addEventListener('offline', updateNetworkStatus, false)
+  })
+
+  //To update network status
+  function updateNetworkStatus() {
+    if (navigator.onLine) {
+      metaTagTheme.setAttribute('content', '#0288d1')
+      headerElement.classList.remove('app__offline')
+    } else {
+      // toast('App is offline')
+      metaTagTheme.setAttribute('content', '#6b6b6b')
+      headerElement.classList.add('app__offline')
+    }
+  }
+})()
