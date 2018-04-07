b063ceb69cf5500e5cd416bacbd0facde38623c9
Christoph Grabo <asaaki@mannaz.cc>
2018-04-08 00:28:42 +0200

    Add some snippets

--- site/static/js/_toolbox.js
+++ site/static/js/_toolbox.js
@@ -11,3 +11,26 @@ const promiseAny = (promises) => {
   });
 };
 // @see <https://github.com/w3c/ServiceWorker/issues/838#issuecomment-191226678>
+
+// from: https://medium.com/@MateMarschalko/online-and-offline-events-with-javascript-d424bec8f43
+function handleConnectionChange(event) {
+  if (event.type == "offline") {
+    console.log("You lost connection.");
+  }
+  if (event.type == "online") {
+    console.log("You are now back online.");
+  }
+
+  console.log(new Date(event.timeStamp));
+}
+window.addEventListener('online', handleConnectionChange);
+window.addEventListener('offline', handleConnectionChange);
+
+// <progress value="0" max="0"><!-- display value not needed --></progress>
+const scrollProgress = document.querySelector('.scrollbar');
+const scrollMaxHeight = () => { return window.scrollMaxY || (document.documentElement.scrollHeight - document.documentElement.clientHeight); };
+const scrollHeight = () => { return window.pageYOffset; };
+const updateProgressWithScrollHeight = () => {
+
+}
+window.onscroll = () => { updateProgressWithScrollHeight(); };
