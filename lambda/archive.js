f75e81d250fa31a9a6cc2f0a8ae446327703c2e2
Christoph Grabo <asaaki@mannaz.cc>
2018-04-08 03:00:31 +0200

    Update archive function

--- lambda/archive.js
+++ lambda/archive.js
@@ -1,28 +1,21 @@
 export function handler (event, context, callback) {
-  const sgKey = process.env.SENDGRID_API_KEY;
-  const sgTo = process.env.SENDGRID_TO;
-  const sgMail = require('@sendgrid/mail');
-
-  sgMail.setApiKey(sgKey);
-
   console.log('[archive.js] triggered!');
-  console.log('event', event);
-  console.log('context', context);
+  const request = require('request');
 
   if(event.body) {
-    const sEvent = JSON.stringify(event);
-    const sContext = JSON.stringify(context);
-    const msg = {
-      to: sgTo,
-      from: 'noreply+netlify-deployment@markentier.tech',
-      subject: 'Deployment happened!',
-      text: `Event: ${sEvent}\nContext: ${sContext}\n\nBye!`,
-      html: `<p>Event: ${sEvent}\nContext: ${sContext}\n\nBye!<p>`
+    const body = JSON.parse(event.body || '{}')
+    const url = body.url;
+    if (url) {
+      request.post(`https://web.archive.org/save/${url}`, function (error, response, body) {
+        const statusCode = response && response.statusCode;
+        console.log('+ url:', url);
+        console.log('+ error:', error);
+        console.log('+ statusCode:', statusCode);
+        callback(null, { statusCode: statusCode, body: 'Sent' });
+      })
+    } else {
+      callback(null, { statusCode: 400, body: 'URL is empty' });
     };
-    console.log(`[SG-MSG]\n${JSON.stringify(msg)}`);
-    sgMail.send(msg);
-
-    callback(null, { statusCode: 200, body: 'Sent' });
   } else {
     callback(null, { statusCode: 400, body: 'No payload given' });
   };
