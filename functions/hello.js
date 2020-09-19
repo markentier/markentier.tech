4e719f219b069d569041e0bd6ca53c83a89dab97
Christoph Grabo <asaaki@mannaz.cc>
2020-09-19 19:47:44 +0200

    Remove functions
    Never used them anyway.

--- functions/hello.js
+++
@@ -1,8 +0,0 @@
-export function handler (event, context, callback) {
-  console.log('event.body', event.body)
-
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!' })
-  })
-}
