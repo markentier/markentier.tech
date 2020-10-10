43f45d54e3959336394c06bf6781fb101d2c54b5
Christoph Grabo <asaaki@mannaz.cc>
2020-10-10 18:23:11 +0200

    Remove service worker completely

--- site/themes/mttt/static/js/mtt.js
+++ site/themes/mttt/static/js/mtt.js
@@ -1,33 +1,18 @@
 "use strict";
 
-((w, n, d, c) => {
-  // REMOVAL stage 1 - 2020-10-08
-  // SERVICE WORKER
-  const registerSW = () => {
-    n.serviceWorker.register("/js/sw.js", { scope: "/" }).then(
-      (reg) => {
-        reg.update();
-      },
-      (_err) => {}
-    );
-  };
-
-  if ("serviceWorker" in n) {
-    d.addEventListener("DOMContentLoaded", (_event) => { registerSW(); });
-  }
-
-  // remove the background image styling, so transparent images won't have
-  // strange SQIP artefacts shining through
-  d.querySelectorAll(
-    "img[loading=lazy][class]:not(.thumbnail):not(.loaded)"
-  ).forEach((img) => {
+// remove the background image styling, so transparent images won't have
+// strange SQIP artefacts shining through
+document
+  .querySelectorAll("img[loading=lazy][class]:not(.thumbnail):not(.loaded)")
+  .forEach((img) => {
     img.onload = (_event) => (img.className = "loaded");
   });
-  d.querySelectorAll("img[loading=lazy].thumbnail:not(.loaded)").forEach(
-    (img) => {
-      img.onload = (_event) => (img.className = "thumbnail loaded");
-    }
-  );
+document
+  .querySelectorAll("img[loading=lazy].thumbnail:not(.loaded)")
+  .forEach((img) => {
+    img.onload = (_event) => (img.className = "thumbnail loaded");
+  });
 
-  w.markentier = { tech: "🦄" }; // ;-)
-})(window, navigator, document, console);
+document.addEventListener("DOMContentLoaded", (_event) => {
+  window.markentier = { tech: "🦄" }; // ;-)
+});
--- site/themes/mttt/static/js/sw.js
+++
@@ -1,7 +0,0 @@
-/* global self, fetch */
-"use strict";
-// REMOVAL stage 1 - 2020-10-08
-// simple pass-through
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
-});
