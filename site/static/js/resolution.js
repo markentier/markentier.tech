f90c8b50e965e7bee1294bb4b6cd7ee346568ca3
Christoph Grabo <asaaki@mannaz.cc>
2020-10-10 18:25:04 +0200

    Refactor resolution script

--- site/static/js/resolution.js
+++ site/static/js/resolution.js
@@ -1,48 +1,51 @@
 "use strict";
-(() => {
-  const [d, s, _w] = [document, screen, window];
 
-  const resHandler = () => {
-    const [w, h, dpr, iw, ih] = [
-      s.width,
-      s.height,
-      _w.devicePixelRatio.toFixed(3),
-      _w.innerWidth,
-      _w.innerHeight,
-    ];
-    const resScreenE = d.querySelector("#resScreen");
-    const resTrueScreenE = d.querySelector("#resTrueScreen");
-    const resDprE = d.querySelectorAll(".resDPR");
-    const resInnerE = d.querySelector("#resInner");
-    const resTrueInnerE = d.querySelector("#resTrueInner");
-    const ppiBox = d.querySelector("#ppi");
-    const ppi = ppiBox.offsetWidth;
-    const cPpiE = d.querySelector("#cPPI");
-    const rounder = (v) => v.toFixed(0);
+const [d, s, W] = [document, screen, window];
 
-    resScreenE.innerHTML = `${w} x ${h}`;
-    resTrueScreenE.innerHTML = `${rounder(w * dpr)} x ${rounder(h * dpr)}`;
-    resDprE.forEach((e) => (e.innerHTML = `${dpr}`));
-    resInnerE.innerHTML = `${iw} x ${ih}`;
-    resTrueInnerE.innerHTML = `${rounder(iw * dpr)} x ${rounder(ih * dpr)}`;
-    cPpiE.innerHTML = `${ppi} ppi`;
+const dqs = (selector) => d.querySelector(selector);
+const dqsa = (selector) => d.querySelectorAll(selector);
+const dce = (tag) => d.createElement(tag);
 
-    d.querySelectorAll(".resValue").forEach((e) => {
-      e.style.fontWeight = "bold";
-      const parent = e.parentNode;
-      if(parent.tagName === "P") {
-        const html = parent.innerHTML;
-        const newE = d.createElement("pre");
-        const newCode = d.createElement("code");
-        newCode.innerHTML = html;
-        newCode.style.fontSize = "133%";
-        newE.appendChild(newCode);
-        parent.replaceWith(newE);
-      }
-    });
-  };
+const resHandler = () => {
+  const [w, h, dpr, iw, ih] = [
+    s.width,
+    s.height,
+    W.devicePixelRatio.toFixed(3),
+    W.innerWidth,
+    W.innerHeight,
+  ];
+  const resScreenE = dqs("#resScreen");
+  const resTrueScreenE = dqs("#resTrueScreen");
+  const resDprE = dqsa(".resDPR");
+  const resInnerE = dqs("#resInner");
+  const resTrueInnerE = dqs("#resTrueInner");
+  const ppiBox = dqs("#ppi");
+  const ppi = ppiBox.offsetWidth;
+  const cPpiE = dqs("#cPPI");
+  const rounder = (v) => v.toFixed(0);
 
-  _w.addEventListener("resize", resHandler)
-  _w.addEventListener("mousewheel", resHandler)
-  setTimeout(resHandler, 0);
-})();
+  resScreenE.innerHTML = `${w} x ${h}`;
+  resTrueScreenE.innerHTML = `${rounder(w * dpr)} x ${rounder(h * dpr)}`;
+  resDprE.forEach((e) => (e.innerHTML = `${dpr}`));
+  resInnerE.innerHTML = `${iw} x ${ih}`;
+  resTrueInnerE.innerHTML = `${rounder(iw * dpr)} x ${rounder(ih * dpr)}`;
+  cPpiE.innerHTML = `${ppi} ppi`;
+
+  dqsa(".resValue").forEach((e) => {
+    e.style.fontWeight = "bold";
+    const parent = e.parentNode;
+    if(parent.tagName === "P") {
+      const html = parent.innerHTML;
+      const newE = dce("pre");
+      const newCode = dce("code");
+      newCode.innerHTML = html;
+      newCode.style.fontSize = "133%";
+      newE.appendChild(newCode);
+      parent.replaceWith(newE);
+    }
+  });
+};
+
+W.addEventListener("resize", resHandler)
+W.addEventListener("mousewheel", resHandler)
+resHandler();
