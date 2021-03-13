212c2024d55f0e68b8aba9b6b71905e9232ce7a3
Christoph Grabo <asaaki@mannaz.cc>
2021-03-13 13:38:02 +0100

    Add more info to the log

--- functions/beacon_img/index.js
+++ functions/beacon_img/index.js
@@ -4,12 +4,20 @@ const BEACON_SVG = '<svg xmlns="http://www.w3.org/2000/svg"/>';
 
 exports.handler = async (event, context) => {
   const qs = event.queryStringParameters || {};
-  const url = qs.u || "(missing)";
   const headers = event.headers || {};
+
+  const url = qs.u || "(missing)";
   const ua = headers["user-agent"] || "(no UA)";
+
+  const clientIp = headers["client-ip"];
+  const secUA = headers["sec-ch-ua"];
+  const netlifyRequestId = headers["x-nf-request-id"];
+
+  const more = { clientIp, secUA, netlifyRequestId }
 
   console.log("[PAGEHIT]", url);
   console.log("[UA]", ua);
+  console.log("[more]", more)
 
   return {
     statusCode: 200,
